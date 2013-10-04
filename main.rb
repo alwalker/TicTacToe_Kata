e8864f1d707a29d3216930f0ad012fe880755ca6
Andrew Walker <alwalker21@gmail.com>
2013-10-03 22:09:03 -0500

    added add player action

--- main.rb
+++ main.rb
@@ -8,3 +8,7 @@ end
 post '/tictactoe/:playerId' do
 	return TicTacToeController.create_new_game(params[:playerId])
 end
+
+put '/tictactoe/:gameId/addPlayer/:playerId' do
+	return TicTacToeController.add_player_to_game(params[:gameId], params[:playerId])
+end
--- tictactoe_controller.rb
+++ tictactoe_controller.rb
@@ -31,4 +31,46 @@ class TicTacToeController
 
 		return [200, game.to_json]
 	end
+
+	def self.add_player_to_game(gameId, playerId)
+		if (playerId.nil? || playerId.empty?)
+			return [500, 'Invalid playerId']
+		end
+		if (gameId.nil? || gameId.empty?)
+			return [500, 'Invalid gameId']
+		end
+
+		begin
+			db = CouchRest.database('https://alwalker.cloudant.com/ttgp_poc_db/')
+		rescue => e
+			@logger.error("Error retrieving database: #{$!}")
+			@logger.error("Backtrace:\n\t#{e.backtrace.join("\n\t")}")
+			return [500, "Couldn't retrieve database"]
+		end		
+		begin
+			game = db.get(gameId)
+		rescue => e
+			@logger.error("Error retrieving database: #{$!}")
+			@logger.error("Backtrace:\n\t#{e.backtrace.join("\n\t")}")
+			return [500, "Couldn't retrieve database"]
+		end
+
+		if (game['players'].length >= 2)
+			return [500, 'Game is already full']
+		end
+		if(game['players'].length == 1 && game['players'][0] == playerId)
+			return [500, 'This player is already playing']
+		end
+		game['players'].push(playerId)
+
+		begin
+			db.save_doc(game)
+		rescue => e
+			@logger.error("Error saving game: #{$!}")
+			@logger.error("Backtrace:\n\t#{e.backtrace.join("\n\t")}")
+			return [500, "Couldn't save game"]
+		end
+
+		return [200, game.to_json]
+	end
 end
